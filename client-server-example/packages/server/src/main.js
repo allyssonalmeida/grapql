32e9684dfb02532c877663ce578f2e1c506df77d
Allysson Fernando Guedes de Almeida <allysson.guedes@trinto.com.br>
2022-05-26 10:24:53 -0300

    atualizações do vídeo 7 - Monorepo

---
+++ client-server-example/packages/server/src/main.js
@@ -0,0 +1,80 @@
+import { createServer } from 'http'
+import { readFile } from 'fs'
+import { resolve } from 'path'
+import { parse } from 'querystring'
+
+const server = createServer((request, response) => {
+  switch(request.url){
+    case '/status': {
+    response.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    response.write(
+      JSON.stringify({
+        status: 'Okay'
+      })
+    );
+    response.end();
+    break;
+    }
+
+    case '/sign-in': {
+      const path = resolve(__dirname, './pages/sign-in.html');
+      console.log(path);
+
+      readFile(path, (error, file) => {
+        if(error){
+          response.writeHead(500, 'Can\'t process HTML file');
+          response.end();
+          return;
+        }
+        response.writeHead(200);
+        response.write(file);
+        response.end();
+      })
+      break;
+    }
+    case '/authenticate': {
+      let data = '';
+      request.on('data', (chunk) => {
+        data += chunk;
+      });
+      request.on('end', () => {
+        const params = parse(data);
+        console.log(parse(data));
+        response.writeHead(301, {
+          Location: '/home'
+        });
+        response.end();
+      });
+      break;
+    }
+    case '/home': {
+      const path = resolve(__dirname, './pages/home.html');
+      console.log(path);
+
+      readFile(path, (error, file) => {
+        if(error){
+          response.writeHead(500, 'Can\'t process HTML file');
+          response.end();
+          return;
+        }
+        response.writeHead(200);
+        response.write(file);
+        response.end();
+      })
+      break;
+    }
+    default: {
+      response.writeHead(404);
+      response.end(); 
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
+})
