594a59c30ffb70f97b1a1164ade672583f98423f
Allysson Fernando Guedes de Almeida <allysson.guedes@trinto.com.br>
2022-05-26 15:26:33 -0300

    Aula 14 - Apollo Server

--- client-server-example/packages/server/src/main.js
+++ client-server-example/packages/server/src/main.js
@@ -1,42 +1,62 @@
-import { createServer } from 'http'
-import { parse } from 'querystring'
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
 
-const server = createServer((request, response) => {
-  switch(request.url){
-    case '/status': {
-    response.writeHead(200, {
-      'Content-type': 'application/json'
-    });
-    response.write(
-      JSON.stringify({
-        status: 'Okay'
-      })
-    );
-    response.end();
-    break;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Client {
+      id: ID!
+      name: String!
     }
 
-    case '/authenticate': {
-      let data = '';
-      request.on('data', (chunk) => {
-        data += chunk;
-      });
-      request.on('end', () => {
-        const params = parse(data);
-        response.end();
-      });
-      break;
+    type Demand {
+      id: ID!
+      name: String!
+      client: Client!
+      deadline: String
     }
-    default: {
-      response.writeHead(404);
-      response.end(); 
+
+    type Query {
+      demands: [Demand]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      demands: () => []
     }
   }
-});
+})
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+})
+// server.get('/status', (_, response) => {
+//   response.send({
+//     status: 'Okay'
+//   })
+// })
+
+// const enableCors = cors({
+//   origin: 'http://localhost:3000'
+// })
+
+// server
+//   .options('/authenticate', enableCors)
+//   .post('/authenticate', enableCors, express.json(), (request, response) => {
+//   console.log(request.body.email, request.body.password)
+//   response.send({
+//     status: 'okay'
+//   });
+// })
+
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listening at http://${HOSTNAME}:${PORT}`);
 })
--- client-server-example/packages/web/src/pages/Signin.js
+++ client-server-example/packages/web/src/pages/Signin.js
@@ -1,17 +1,51 @@
 import React from 'react';
 
-// import { Container } from './styles';
 
 function Signin() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  function handleSubmit(e){
+    e.preventDefault();
+
+    fetch('http://localhost:8000/authenticate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      }),
+    })
+    .then((response) => response.json())
+    .then(() => {
+      console.log("SUCESSO");
+    });
+
+  }
+
   return(
-    <form action="authenticate" method="POST">
+    <form action="authenticate" method="POST" onSubmit={handleSubmit}>
       <fieldset>
-        <label for="email">E-mail</label>
-        <input type="email" name="email" id="email" inputmode="email" autocomplete="username" />
+        <label htmlFor="email">E-mail</label>
+        <input 
+          type="email"
+          id="email" 
+          inputMode="email" 
+          autoComplete="username"
+          onChange={({target}) => {setEmail(target.value)}}
+          value={email}
+        />
       </fieldset>
       <fieldset>
-        <label for="password">Senha</label>
-        <input type="password" name="password" id="password" autocomplete="current-password" />
+        <label htmlFor="password">Senha</label>
+        <input 
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          onChange={({target}) => {setPassword(target.value)}}
+          value={password}
+        />
       </fieldset>
       <button type="submit">Entrar</button>
     </form>
